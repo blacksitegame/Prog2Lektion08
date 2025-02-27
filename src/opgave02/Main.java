d657c7c7bf1899cbb4d093bc27ef34189527f357
Benn Christensen <benn.christensen@wolterskluwer.com>
2025-02-27 09:35:51 +0100

    Added exercise 2

---
+++ src/opgave02/Main.java
@@ -0,0 +1,15 @@
+package opgave02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>(List.of(0, 14, 0, 42, 0, -1, 0 , 101));
+        int numberOfZeros = countZeros(integers);
+    }
+
+    private static int countZeros(ArrayList<Integer> integers) {
+        return 0;
+    }
+}
