149d96151db386e979a05599805ef4b292eea99b
Benn Christensen <benn.christensen@wolterskluwer.com>
2025-03-01 17:16:18 +0100

    Added exercise 4

---
+++ src/opgave04/Main.java
@@ -0,0 +1,23 @@
+package opgave04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>(List.of(15, 0, -23, 42, 101, Integer.MIN_VALUE, 202, 0, -3));
+        System.out.println(mergeSort(integers));
+    }
+
+    public static ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
+        //TODO
+        return null;
+    }
+
+
+    private static ArrayList<Integer> merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        //TODO
+        return null;
+    }
+
+}
